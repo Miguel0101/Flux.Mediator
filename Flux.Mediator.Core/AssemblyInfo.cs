f704f7047f8b780963d9a16f93ad0c8c4a252b61
Miguel0101 <miguelmagnofn@gmail.com>
2025-12-29 17:14:24 -0300

    update(refs): Update references

--- Flux.Mediator.Core/AssemblyInfo.cs
+++ Flux.Mediator.Core/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Flux.Mediator")]
+[assembly: InternalsVisibleTo("Flux.Mediator.Extensions.DependencyInjection")]
 
 namespace Flux.Mediator.Core;
